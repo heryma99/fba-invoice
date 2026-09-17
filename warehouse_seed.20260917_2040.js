e8818221cdcbccc8c8e2df41eebe4d231bebb239
heryma99 <heryma99@gmail.com>
2026-09-17 20:35:24 +0800

    deploy warehouse_seed.20260917_2040.js

---
+++ warehouse_seed.20260917_2040.js
@@ -0,0 +1,68 @@
+// Amazon FBA 官方履约中心（FC）目录种子
+// 数据来源：Amazon 公开的 FBA 仓代码 + 地址（社区从官方目录整理，已联网核实 2026-07-27）
+// 用途：仓库主数据预置，覆盖用户实际发货国家。可按「仓库主数据」页增删改。
+// 注意：公司名统一用 "Amazon <FC代码>"，如需按模板区分（Amazon.com / Amazon.co.uk 等）可在主数据页改。
+window.WAREHOUSE_SEED = [
+  // ===== 美国 US =====
+  {id:'wh_SCK8',代码:'SCK8',国家:'美国',公司:'Amazon SCK8',省份:'CA',城市:'Stockton',地址:'Stockton, CA',邮编:'95206',电话:''},
+  {id:'wh_TCY1',代码:'TCY1',国家:'美国',公司:'Amazon TCY1',省份:'CA',城市:'Stockton',地址:'2690 East Arch Airport Road',邮编:'95206',电话:''},
+  {id:'wh_ONT8',代码:'ONT8',国家:'美国',公司:'Amazon ONT8',省份:'CA',城市:'Moreno Valley',地址:'24300 Nandina Ave',邮编:'92551',电话:''},
+  {id:'wh_ONT2',代码:'ONT2',国家:'美国',公司:'Amazon ONT2',省份:'CA',城市:'San Bernardino',地址:'1910 E Central Ave',邮编:'92408',电话:''},
+  {id:'wh_LAX9',代码:'LAX9',国家:'美国',公司:'Amazon LAX9',省份:'CA',城市:'Fontana',地址:'11263 Oleander Ave',邮编:'92337',电话:''},
+  {id:'wh_PHX3',代码:'PHX3',国家:'美国',公司:'Amazon PHX3',省份:'AZ',城市:'Phoenix',地址:'6835 W. Buckeye Rd',邮编:'85043',电话:''},
+  {id:'wh_PHX7',代码:'PHX7',国家:'美国',公司:'Amazon PHX7',省份:'AZ',城市:'Phoenix',地址:'800 N 75th Ave',邮编:'85043',电话:''},
+  {id:'wh_OAK3',代码:'OAK3',国家:'美国',公司:'Amazon OAK3',省份:'CA',城市:'Patterson',地址:'255 Park Center Dr',邮编:'95363',电话:''},
+  {id:'wh_OAK4',代码:'OAK4',国家:'美国',公司:'Amazon OAK4',省份:'CA',城市:'Tracy',地址:'1555 N. Chrisman Rd',邮编:'95304',电话:''},
+  // ===== 沙特 SA =====
+  {id:'wh_RUH8',代码:'RUH8',国家:'沙特',公司:'Amazon RUH8',省份:'',城市:'Riyadh',地址:'RUH8, Riyadh',邮编:'',电话:''},
+  // ===== 德国 DE =====
+  {id:'wh_FRA1',代码:'FRA1',国家:'德国',公司:'Amazon FRA1',省份:'HE',城市:'Bad Hersfeld',地址:'Am Schloß Eichhof 1',邮编:'36251',电话:''},
+  {id:'wh_FRA3',代码:'FRA3',国家:'德国',公司:'Amazon FRA3',省份:'HE',城市:'Bad Hersfeld',地址:'Amazonstraße 1',邮编:'36251',电话:''},
+  {id:'wh_LEJ1',代码:'LEJ1',国家:'德国',公司:'Amazon LEJ1',省份:'SN',城市:'Leipzig',地址:'Amazonstraße 1',邮编:'04347',电话:''},
+  {id:'wh_DUS2',代码:'DUS2',国家:'德国',公司:'Amazon DUS2',省份:'NRW',城市:'Rheinberg',地址:'Amazonstraße 1 / Alte Landstraße',邮编:'47495',电话:''},
+  {id:'wh_MUC3',代码:'MUC3',国家:'德国',公司:'Amazon MUC3',省份:'BY',城市:'Graben',地址:'Zeppelinstraße 2',邮编:'86836',电话:''},
+  {id:'wh_CGN1',代码:'CGN1',国家:'德国',公司:'Amazon CGN1',省份:'RP',城市:'Kobern-Gondorf',地址:'Amazonstraße 1',邮编:'56330',电话:''},
+  {id:'wh_DTM2',代码:'DTM2',国家:'德国',公司:'Amazon DTM2',省份:'NRW',城市:'Dortmund',地址:'Kaltband Straße 4',邮编:'44145',电话:''},
+  {id:'wh_HAM2',代码:'HAM2',国家:'德国',公司:'Amazon HAM2',省份:'NI',城市:'Winsen',地址:'Borgwardstraße 10',邮编:'21423',电话:''},
+  {id:'wh_STR1',代码:'STR1',国家:'德国',公司:'Amazon STR1',省份:'BW',城市:'Pforzheim',地址:'Amazonstraße 1',邮编:'75177',电话:''},
+  {id:'wh_BER3',代码:'BER3',国家:'德国',公司:'Amazon BER3',省份:'BB',城市:'Brieselang',地址:'Brieselang',邮编:'14656',电话:''},
+  // ===== 阿联酋 AE =====
+  {id:'wh_DXB1',代码:'DXB1',国家:'阿联酋',公司:'Amazon DXB1',省份:'',城市:'Dubai',地址:'Souq Warehouse, Dubai Investment Park 2, Behind ENOC petrol station',邮编:'',电话:''},
+  {id:'wh_DXB2',代码:'DXB2',国家:'阿联酋',公司:'Amazon DXB2',省份:'',城市:'Dubai',地址:'Plot TP10104C/D, CEVA Logistics, National Industries Park',邮编:'',电话:''},
+  {id:'wh_DXB3',代码:'DXB3',国家:'阿联酋',公司:'Amazon DXB3',省份:'',城市:'Dubai',地址:'Amazon fulfillment centre, Dubai Logistics City',邮编:'',电话:''},
+  // ===== 日本 JP =====
+  {id:'wh_NRT1',代码:'NRT1',国家:'日本',公司:'Amazon NRT1',省份:'千葉県',城市:'市川',地址:'市川市塩浜2-13-1',邮编:'272-0193',电话:''},
+  {id:'wh_NRT2',代码:'NRT2',国家:'日本',公司:'Amazon NRT2',省份:'千葉県',城市:'八千代',地址:'八千代市上高野2036',邮编:'276-8525',电话:''},
+  {id:'wh_NRT5',代码:'NRT5',国家:'日本',公司:'Amazon NRT5',省份:'埼玉県',城市:'川越',地址:'川越市南台1-10-15',邮编:'350-1182',电话:''},
+  {id:'wh_HND3',代码:'HND3',国家:'日本',公司:'Amazon HND3',省份:'埼玉県',城市:'川島',地址:'比企郡川島町かわじま2-1-1',邮编:'350-0195',电话:''},
+  {id:'wh_KIX1',代码:'KIX1',国家:'日本',公司:'Amazon KIX1',省份:'大阪府',城市:'堺',地址:'堺市堺区築港八幡町138-7',邮编:'590-8589',电话:''},
+  {id:'wh_KIX2',代码:'KIX2',国家:'日本',公司:'Amazon KIX2',省份:'大阪府',城市:'大東',地址:'大東市緑が丘2-1-1',邮编:'574-8531',电话:''},
+  {id:'wh_NGO2',代码:'NGO2',国家:'日本',公司:'Amazon NGO2',省份:'岐阜県',城市:'多治見',地址:'多治見市旭ヶ丘10-6',邮编:'507-8585',电话:''},
+  {id:'wh_HSG1',代码:'HSG1',国家:'日本',公司:'Amazon HSG1',省份:'佐賀県',城市:'鳥栖',地址:'鳥栖市弥生が丘3-1-3',邮编:'841-8505',电话:''},
+  {id:'wh_FSZ1',代码:'FSZ1',国家:'日本',公司:'Amazon FSZ1',省份:'神奈川県',城市:'小田原',地址:'小田原市扇町4-5-1',邮编:'250-8560',电话:''},
+  // ===== 加拿大 CA =====
+  {id:'wh_YYZ1',代码:'YYZ1',国家:'加拿大',公司:'Amazon YYZ1',省份:'ON',城市:'Mississauga',地址:'6363 Millcreek Dr',邮编:'L5N 1L8',电话:''},
+  {id:'wh_YYZ2',代码:'YYZ2',国家:'加拿大',公司:'Amazon YYZ2',省份:'ON',城市:'Milton',地址:'2750 Peddie Rd',邮编:'L9T 6Y9',电话:''},
+  {id:'wh_YYZ3',代码:'YYZ3',国家:'加拿大',公司:'Amazon YYZ3',省份:'ON',城市:'Brampton',地址:'7995 Winston Churchill Blvd',邮编:'L6Y 0B2',电话:''},
+  {id:'wh_YVR1',代码:'YVR1',国家:'加拿大',公司:'Amazon YVR1',省份:'BC',城市:'Delta',地址:'450 Derwent Pl',邮编:'V3M 5Y9',电话:''},
+  {id:'wh_YYC1',代码:'YYC1',国家:'加拿大',公司:'Amazon YYC1',省份:'AB',城市:'Balzac',地址:'293069 Colonel Robertson Way',邮编:'T4A 1C6',电话:''},
+  {id:'wh_YOW1',代码:'YOW1',国家:'加拿大',公司:'Amazon YOW1',省份:'ON',城市:'Ottawa',地址:'5225 Boundary Rd',邮编:'K4N 1P6',电话:''},
+  {id:'wh_YEG1',代码:'YEG1',国家:'加拿大',公司:'Amazon YEG1',省份:'AB',城市:'Edmonton',地址:'1440 39 Ave',邮编:'T6W 1A7',电话:''},
+  // ===== 英国 UK =====
+  {id:'wh_EDI4',代码:'EDI4',国家:'英国',公司:'Amazon EDI4',省份:'Fife',城市:'Dunfermline',地址:'Amazon Way',邮编:'KY11 8ST',电话:''},
+  {id:'wh_LTN1',代码:'LTN1',国家:'英国',公司:'Amazon LTN1',省份:'Beds',城市:'Ridgmont',地址:'Marston Gate Fulfillment Centre',邮编:'MK43 0ZA',电话:''},
+  {id:'wh_BHX4',代码:'BHX4',国家:'英国',公司:'Amazon BHX4',省份:'Coventry',城市:'Coventry',地址:'Plot 1, Lyons Park, Coundon Wedge Drive',邮编:'CV5 9FA',电话:''},
+  {id:'wh_MAN1',代码:'MAN1',国家:'英国',公司:'Amazon MAN1',省份:'Manchester',城市:'Altrincham',地址:'6 Sunbank Lane, Airport City',邮编:'M90 5AA',电话:''},
+  {id:'wh_BHX1',代码:'BHX1',国家:'英国',公司:'Amazon BHX1',省份:'Staffordshire',城市:'Rugeley',地址:'Towers Business Park, Power Station Road',邮编:'WS15 1NZ',电话:''},
+  {id:'wh_LBA2',代码:'LBA2',国家:'英国',公司:'Amazon LBA2',省份:'Doncaster',城市:'Doncaster',地址:'Unit 1, Iport Avenue, New Rossington',邮编:'DN11 0BG',电话:''},
+  {id:'wh_LTN2',代码:'LTN2',国家:'英国',公司:'Amazon LTN2',省份:'Herts',城市:'Hemel Hempstead',地址:'Boundary Way',邮编:'HP2 7LF',电话:''},
+  {id:'wh_CWL1',代码:'CWL1',国家:'英国',公司:'Amazon CWL1',省份:'Swansea',城市:'Swansea',地址:'Ffordd Amazon, Crymlyn Burrows',邮编:'SA1 8QX',电话:''},
+  // ===== 澳大利亚 AU =====
+  {id:'wh_BNE1',代码:'BNE1',国家:'澳大利亚',公司:'Amazon BNE1',省份:'QLD',城市:'Lytton',地址:'42-52 Export Street',邮编:'4178',电话:''},
+  {id:'wh_BWU1',代码:'BWU1',国家:'澳大利亚',公司:'Amazon BWU1',省份:'NSW',城市:'Moorebank',地址:'23 Centenary Avenue',邮编:'',电话:''},
+  {id:'wh_BWU2',代码:'BWU2',国家:'澳大利亚',公司:'Amazon BWU2',省份:'NSW',城市:'Kemps Creek',地址:'13 Emporium Avenue',邮编:'',电话:''},
+  {id:'wh_MEL1',代码:'MEL1',国家:'澳大利亚',公司:'Amazon MEL1',省份:'VIC',城市:'Dandenong South',地址:'29 National Dr',邮编:'3175',电话:''},
+  {id:'wh_MEL5',代码:'MEL5',国家:'澳大利亚',公司:'Amazon MEL5',省份:'VIC',城市:'Ravenhall',地址:'103 Palm Springs Rd',邮编:'3023',电话:''},
+  {id:'wh_PER3',代码:'PER3',国家:'澳大利亚',公司:'Amazon PER3',省份:'WA',城市:'Perth Airport',地址:'60 Paltridge Road',邮编:'6105',电话:''},
+  {id:'wh_XAU1',代码:'XAU1',国家:'澳大利亚',公司:'Amazon XAU1',省份:'VIC',城市:'Tullamarine',地址:'25-29 Jets Court',邮编:'3043',电话:''}
+];
